8301d0d934ac8a27749d61b1ab14505fcdd31d98
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-05-14 10:10:44 +0600

    home design start

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
 const Home = () => {
   return (
-    <div>Home page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover & Share
+            <br className="md:hidden" />
+            <span className="orange_gradient"> AI-Powered Prompts</span>
+        </h1>
+
+        <p className="desc text-center">
+            Promptopia is an open source AI Prompting 
+            tool for modern world to discover,create and share 
+            creative prompts
+        </p>
+
+        
+    </section>
   )
 }
 
